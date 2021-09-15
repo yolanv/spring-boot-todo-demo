21886f20e911c465405ce7cfa3c216b67d1b0938
Yolan Vloeberghs <yolanvloeberghs@gmail.com>
2021-09-15 19:30:13 +0200

    Add environment variable "DOCKER_ENV" in logging for demo purposes

--- src/main/java/be/yolanv/tododemo/SpringBootTodoDemoApplication.java
+++ src/main/java/be/yolanv/tododemo/SpringBootTodoDemoApplication.java
@@ -1,13 +1,23 @@
 package be.yolanv.tododemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootTodoDemoApplication {
+public class SpringBootTodoDemoApplication implements CommandLineRunner {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootTodoDemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTodoDemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		if (System.getenv("DOCKER_ENV") != null) {
+			LOGGER.info("{}", System.getenv("DOCKER_ENV"));
+		}
+	}
 }
